ea0c683c212cb96ecd9d9f7d6fdb5cf5a6e3df59
dymafr <erwan1309@hotmail.com>
2021-10-02 11:46:05 +0200

    1 file modified

--- src/app/shared/store/todos.reducer.ts
+++ src/app/shared/store/todos.reducer.ts
@@ -1,5 +1,6 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { Todo } from '../interfaces/todo.interface';
+import * as TodosActions from './todos.actions';
 
 export interface TodosState {
   data: Todo[];
@@ -14,4 +15,35 @@ export const TODOS_INITIAL_STATE = {
   ],
 };
 
-export const todosReducer = createReducer(TODOS_INITIAL_STATE);
+export const todosReducer = createReducer(
+  TODOS_INITIAL_STATE,
+  on(
+    TodosActions.addTodoAction,
+    (state: TodosState, { todo }: { todo: Todo }): TodosState => {
+      return {
+        ...state,
+        data: [...state.data, todo],
+      };
+    }
+  ),
+  on(
+    TodosActions.deleteTodoAction,
+    (state: TodosState, { index }: { index: number }): TodosState => {
+      return {
+        ...state,
+        data: state.data.filter((v, i) => i !== index),
+      };
+    }
+  ),
+  on(
+    TodosActions.toggleTodoAction,
+    (state: TodosState, { index }: { index: number }): TodosState => {
+      return {
+        ...state,
+        data: state.data.map((v, i) =>
+          i !== index ? v : { ...v, done: !v.done }
+        ),
+      };
+    }
+  )
+);

c1038aa2bd7c65a670911b8c84655ec45c6d76c6
dymafr <erwan1309@hotmail.com>
2021-10-02 12:03:49 +0200

    1 file modified, 1 added, 1 deleted

--- src/app/shared/store/todos.reducer.ts
+++ src/app/shared/store/todos.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { Todo } from '../interfaces/todo.interface';
-import * as TodosActions from './todos.action';
+import * as TodosActions from './todos.actions';
 
 export interface TodosState {
   data: Todo[];

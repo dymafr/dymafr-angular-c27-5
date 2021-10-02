456218ffc7a5f6380fc5468733615ac711f8bb71
dymafr <erwan1309@hotmail.com>
2021-10-02 11:34:45 +0200

    1 file modified, 1 added

--- src/app/shared/store/index.ts
+++ src/app/shared/store/index.ts
@@ -1,23 +1,12 @@
 import { Action, ActionReducerMap } from '@ngrx/store';
-import { createReducer } from '@ngrx/store';
 import { Todo } from '../interfaces/todo.interface';
+import { todosReducer } from './todo.reducer';
 
 export interface AppState {
   todos: {
     data: Todo[];
   };
 }
-
-const TODOS_INITIAL_STATE = {
-  data: [
-    {
-      message: 'Manger une pizza',
-      done: false,
-    },
-  ],
-};
-
-const todosReducer = createReducer(TODOS_INITIAL_STATE);
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState, Action> = {
   todos: todosReducer,
---
+++ src/app/shared/store/todo.reducer.ts
@@ -0,0 +1,17 @@
+import { createReducer, on } from '@ngrx/store';
+import { Todo } from '../interfaces/todo.interface';
+
+export interface TodosState {
+  data: Todo[];
+}
+
+export const TODOS_INITIAL_STATE = {
+  data: [
+    {
+      message: 'Manger une pizza',
+      done: false,
+    },
+  ],
+};
+
+export const todosReducer = createReducer(TODOS_INITIAL_STATE);
